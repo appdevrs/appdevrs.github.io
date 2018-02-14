940a8ebb5c6cf56a56e197b34e7875db5f31118f
appdevrs <36398747+appdevrs@users.noreply.github.com>
2018-02-14 13:17:06 +0100

    Update twittererror.js

--- js/twittererror.js
+++ js/twittererror.js
@@ -3,7 +3,7 @@ if (elems.length >= 1) {
   var br        = document.createElement("br");
   var span      = document.createElement("span");
   var textnode  = document.createTextNode("private mode of your browser is blocking the Twitter Timeline from loading.");
-  node.appendChild(textnode);
+  span.appendChild(textnode);
   elems[0].parentNode.insertBefore(span, elems[0].nextsibling);
   elems[0].parentNode.insertBefore(br, elems[0].nextsibling);
 }
